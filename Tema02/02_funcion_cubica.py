84160a7ea21bc6579a588d877d85ad9a8071bc6d
ItaliaYLO <163441749+ItaliaYLO@users.noreply.github.com>
2026-05-17 22:02:25 -0600

    Update 02_funcion_cubica.py

--- Tema02/02_funcion_cubica.py
+++ Tema02/02_funcion_cubica.py
@@ -1,1 +1,14 @@
+from biseccion_base import biseccion
 
+# Definimos la función cúbica
+def f(x):
+    return x**3 - x - 2
+
+if __name__ == "__main__":
+    print("=== CASO 02: FUNCIÓN CÚBICA ===")
+    print("Función: f(x) = x^3 - x - 2\n")
+    
+    intervalo_a = 1.0
+    intervalo_b = 2.0
+    
+    biseccion(f, intervalo_a, intervalo_b, tol=1e-5)
