f9397c51ccf41525a1bae3cad1805ca513e57398
ItaliaYLO <163441749+ItaliaYLO@users.noreply.github.com>
2026-05-17 22:06:52 -0600

    Update 05_funcion_logaritmica.py

--- Tema02/05_funcion_logaritmica.py
+++ Tema02/05_funcion_logaritmica.py
@@ -1,1 +1,15 @@
+import math
+from biseccion_base import biseccion
 
+# Definimos la función logarítmica
+def f(x):
+    return math.log(x) + x - 2
+
+if __name__ == "__main__":
+    print("=== CASO 05: FUNCIÓN LOGARÍTMICA ===")
+    print("Función: f(x) = ln(x) + x - 2\n")
+    
+    intervalo_a = 1.0
+    intervalo_b = 2.0
+    
+    biseccion(f, intervalo_a, intervalo_b, tol=1e-5)
