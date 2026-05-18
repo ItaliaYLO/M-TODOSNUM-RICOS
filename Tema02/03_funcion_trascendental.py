d8bb51d9482b6ff60a436ac6466fd0d5896290ce
ItaliaYLO <163441749+ItaliaYLO@users.noreply.github.com>
2026-05-17 22:02:39 -0600

    Update 03_funcion_trascendental.py

--- Tema02/03_funcion_trascendental.py
+++ Tema02/03_funcion_trascendental.py
@@ -1,1 +1,15 @@
+import math
+from biseccion_base import biseccion
 
+# Definimos la función trigonométrica trascendental
+def f(x):
+    return math.cos(x) - x
+
+if __name__ == "__main__":
+    print("=== CASO 03: FUNCIÓN TRASCENDENTAL ===")
+    print("Función: f(x) = cos(x) - x\n")
+    
+    intervalo_a = 0.0
+    intervalo_b = 1.0
+    
+    biseccion(f, intervalo_a, intervalo_b, tol=1e-5)
